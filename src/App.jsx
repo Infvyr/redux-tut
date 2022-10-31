aa3dafac5ae5e367ecadfb2b3699270987e40b34
Infvyr <allfortrashmail@gmail.com>
2022-10-31 20:45:08 +0200

    refactor error page and run format command

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import MainLayout from 'layout/Main/MainLayout';
 import Sidebar from 'layout/Sidebar';
+import { useRouteError } from 'react-router-dom';
 
 function App() {
+	const error = useRouteError();
+	const errorPageClassName = error ? 'grid-cols-1' : 'md:grid-cols-[300px_1fr]';
+
 	return (
-		<>
+		<div className={`${errorPageClassName} block sm:grid min-h-screen`}>
 			<Sidebar />
 			<MainLayout />
-		</>
+		</div>
 	);
 }
 
--- src/pages/Error/NotFoundPage.jsx
+++ src/pages/Error/NotFoundPage.jsx
@@ -1,10 +1,26 @@
-import { useRouteError } from 'react-router-dom';
+import Button from 'components/Button';
+import Space from 'components/Space';
+import { useNavigate, useRouteError } from 'react-router-dom';
+import styles from './NotFoundPage.module.css';
 
 function NotFoundPage() {
+	const navigate = useNavigate();
 	const error = useRouteError();
-	console.error(error);
+	const { statusText } = error;
 
-	return <div>Page not found</div>;
+	const goHome = () => navigate('/');
+
+	return (
+		<main className={styles.main}>
+			<section className="h-screen flex items-center justify-center flex-col">
+				<h1>{statusText || "Oops! There's an error!"}</h1>
+				<Space />
+				<Button className="btn-primary" onClick={goHome}>
+					Go home
+				</Button>
+			</section>
+		</main>
+	);
 }
 
 export default NotFoundPage;
