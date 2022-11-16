e0f95799f5e82f90812f1eb0c6d1cad45a0e6825
Infvyr <allfortrashmail@gmail.com>
2022-11-16 17:54:54 +0200

    fill home page with content

--- src/pages/Dash/index.jsx
+++ src/pages/Dash/index.jsx
@@ -1,12 +1,27 @@
 import PageHeader from 'components/PageHeader';
+import { useEffect, useState } from 'react';
+import Markdown from 'markdown-to-jsx';
 
 function Dash() {
+	const readmeFile = 'dash.md';
+	const [content, setContent] = useState('');
+
+	useEffect(() => {
+		import(`markdown/${readmeFile}`)
+			.then(res => {
+				fetch(res.default)
+					.then(res => res.text())
+					.then(res => setContent(res));
+			})
+			.catch(err => console.error(err));
+	}, []);
+
 	return (
 		<>
 			<PageHeader backTo={false} title="Dashboard" />
 
 			<div className="card grow">
-				<p>A stats maybe</p>
+				<Markdown>{content}</Markdown>
 			</div>
 		</>
 	);
